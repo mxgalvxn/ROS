3e4485173fbcef552e44528d26c437b693632d3d
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-20 12:10:11 -0600

    Update signal:generator.py

--- Actividad1/challenge1/signal:generator.py
+++ Actividad1/challenge1/signal:generator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+from std_msgs.msg import Float32
 from std_msgs.msg import String
 
 if __name__ == '__main__':
-    pub = rospy.Publisher("time", String, queue_size = 10)
-    pub1 = rospy.Publisher("signal", String, queue_size = 10)
+    pub = rospy.Publisher("time", Float32, queue_size = 10)
+    pub1 = rospy.Publisher("signal", Float32, queue_size = 10)
     rospy.init_node("signal_generator")
     rate = rospy.Rate(10)
 
