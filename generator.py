71c422b8bc6395b28ac9cddc67f9b4cb79565c03
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-19 20:48:22 -0600

    Update generator.py (#1)

--- generator.py
+++ generator.py
@@ -4,7 +4,8 @@ import numpy as np
 from std_msgs.msg import String
 
 if __name__ == '__main__':
-    pub = rospy.Publisher("chatter", String, queue_size = 10)
+    pub = rospy.Publisher("time", String, queue_size = 10)
+    pub1 = rospy.Publisher("signal", String, queue_size = 10)
     rospy.init_node("signal_generator")
     rate = rospy.Rate(10)
 
@@ -12,5 +13,5 @@ if __name__ == '__main__':
        time = rospy.get_time() 
        pub.publish(time)
        signal = np.sin(time)
-       pub.publish(signal)  
+       pub1.publish(signal)  
        rate.sleep()    
