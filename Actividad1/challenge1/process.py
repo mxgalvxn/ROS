80377239436d99aa78433c3dd7edbe814794c845
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-20 12:51:53 -0600

    Update process.py

--- Actividad1/challenge1/process.py
+++ Actividad1/challenge1/process.py
@@ -10,18 +10,18 @@ def callTime(msg):
     rospy.loginfo( " Time: %s", msg.data)
 
 def callSignal1(msg):
-    rospy.loginfo( " Time: %s", msg.data)
+    rospy.loginfo( " Sine: %s", msg.data)
 
 def callSignal(msg):
     global signal_data
     signal_data = msg.data + 10
     signal_data /=2
-    rospy.loginfo( " Sine %s", signal_data)
+    rospy.loginfo( " Sine_modified %s", signal_data)
     
 if __name__ == '__main__':
     rospy.init_node("process")
     rospy.Subscriber("signal", Float32,callSignal1 )
-    rospy.Subscriber("signal modified ", Float32, callSignal)
+    rospy.Subscriber("signal", Float32, callSignal)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
