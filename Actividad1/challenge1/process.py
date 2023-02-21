b7102219daa6b5732223f8caeed521d9bfccfb6a
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-21 12:53:27 -0600

    Update process.py

--- Actividad1/challenge1/process.py
+++ Actividad1/challenge1/process.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         if signal_data < 0:
             signal_data = signal_data*-1
+        signal_data += 10
         signal_data/=2
         pub.publish(signal_data)  
         rate.sleep()
