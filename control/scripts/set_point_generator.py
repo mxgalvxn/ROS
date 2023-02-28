520e20572727f7c87258ce46df9ed6729b4cdc1e
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-27 18:01:05 -0600

    Update set_point_generator.py

--- control/scripts/set_point_generator.py
+++ control/scripts/set_point_generator.py
@@ -6,22 +6,20 @@ import math as m
 from std_msgs import Float32
 
 
-rospy.init_node("setpoint_generator")
-  
-setPointPublisher =rospy.Publisher("/setpoint",Float32, queue_size=10)    
-init_time = rospy.get_time()
+if __name__ == '__main__':
+  rospy.init_node("setpoint_generator")
+    
+  setPointPublisher =rospy.Publisher("/setpoint",Float32, queue_size=10)    
+  init_time = rospy.get_time()
 
-  
-rate = rospy.Rate(1000)
-nodeRate = rospy.get_param("/system_node_rate")
+  rate = rospy.get_param("/system_node_rate")
 
-
-while not rospy.is_shutdown():
+  while not rospy.is_shutdown():
+    
+    time = rospy.get_time()-init_time
+    refVal = abs(3*m.tan(pow(m.log(1/m.exp(m.sin(time*0.3))),3))+m.cos(time*0.3))
+    setPointPublisher.publish(refVal);
+    rospy.loginfo("The signal value is: %f at a time %f", refVal, time)
+    rate.sleep()
   
-  time = rospy.get_time()-init_time
-  refVal = abs(3*m.tan(pow(m.log(1/m.exp(m.sin(time*0.3))),3))+m.cos(time*0.3))
-  setPointPublisher.publish(refVal);
-  rospy.loginfo("The signal value is: %f at a time %f", refVal, time)
-  rate.sleep()
- 
 
