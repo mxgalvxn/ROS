6be36234f03ecd9a210b6fe5e90eec2004245ea3
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-27 18:00:26 -0600

    Update controller_node.py

--- control/scripts/controller_node.py
+++ control/scripts/controller_node.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float32
-from control.msg import motor_output
 from control.msg import motor_input
+from control.msg import motor_output
+
 
 motor_out = 0
 motor_init = False
@@ -19,30 +20,31 @@ def receive_setpoint(data):
 if __name__ == '__main__':
     rospy.init_node("controller_node")
 
-    rate = rospy.Rate(1000)
+    rate = rospy.get_param("/system_node_rate")
     kp = rospy.get_param("/Kp")
     Ti = rospy.get_param("/Ti")
     Td = rospy.get_param("/Td")
     ref = rospy.get_param("/set_point")
 
-    systemfeedback = rospy.Subscriber("/motor_out",Float32, queue_size=10)
-    controllerOutput = rospy.Publisher("/setpoint",Float32, queue_size=10)
-    setPointSubscr = rospy.Subscriber("/motor_out",Float32, queue_size=10)
+    systemfeedback = rospy.Subscriber("/motor_out",Float32, receive_feedback)
+    controllerOutput = rospy.Publisher("/motor_input",Float32, queue_size=10)
+    setPointSubscr = rospy.Subscriber("/motor_out",Float32,receive_setpoint)
     handler=rospy.Subscriber("/setpoint",Float32, queue_size=10)
-    time = rospy.Time.now().to_sec()
+    init_time = rospy.get_time()
     lastError = 0.0
     cumError = 0.0
     motor_in = 0.0
-    controllerOutput.publish(controllerOutput)
-    while rospy.is_shutdown():
+    controllerOutput.publish(motor_in)
+    while not rospy.is_shutdown():
+        time = rospy.get_time()-init_time
         dTime = rospy.Time.now().to_sec() - time
         if dTime > 0 and motor_init:
-            time = rospy.Time.now().toSec()
+            time = rospy.get_time()-init_time
             error = ref - motor_out
             cumError += error*dTime
             rateError = (error - lastError) / dTime
             lastError = error
-            motor_in.input = (kp*error)+(Td*rateError)+(Ti*cumError)
+            motor_in = (kp*error)+(Td*rateError)+(Ti*cumError)
             controllerOutput.publish(motor_in)
         else:
             motor_in = 0
