fe76f568f41199af21c3a5205a22d3bb50559c1f
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-02-28 12:32:43 -0600

    Update controller_node.py

--- control/scripts/controller_node.py
+++ control/scripts/controller_node.py
@@ -4,21 +4,23 @@ from std_msgs.msg import Float32
 from control.msg import motor_input
 from control.msg import motor_output
 
+
 motor_out = 0
 motor_init = False
 ref = 0
 
 def receive_feedback(output):
-    global motor_out 
-    global motor_init 
+    global motor_out
+    global motor_init
     motor_out = output
-    motor_init= True
+    motor_init = True
     
 
-def receive_setpoint(refSetPoint):
-    ref = refSetPoint.data
-    
+def receive_setpoint(msg):
+    global ref
+    ref = msg.data
 
+    
 if __name__ == '__main__':
     rospy.init_node("controller_node")
 
@@ -35,9 +37,10 @@ if __name__ == '__main__':
     Td = rospy.get_param("/Td")
     ref = rospy.get_param("/set_point")
 
-    time = rospy.Time.now().to_sec()
+    init_time = rospy.Time.now().to_sec()
     lastError = 0.0
-    cumError = 0.0
+    totError = 0.0
+    
     motor_in = 0.0
 
     controllerOutput.publish(motor_in, 0)
@@ -45,24 +48,21 @@ if __name__ == '__main__':
     
     
     while not rospy.is_shutdown():
-        dTime = rospy.Time.now().to_sec() - time
+        time = rospy.Time.now().to_sec() - init_time
         
-        if dTime > 0 and motor_init:
+        if time > 0 and motor_init:
             time = rospy.Time.now().to_sec()
             error = ref - motor_out.output
-            cumError += error*dTime
-            rateError = (error - lastError) / dTime
+            totError += error*time
             lastError = error
-            motor_in = (kp*error)+(Td*rateError)+(Ti*cumError)
+            rateError = (error - lastError) / time
+
+            motor_in = (kp*error)+(Td*rateError)+(Ti*totError)
             rospy.loginfo("The motor_in value is: %f", motor_in)
-            controllerOutput.publish(motor_in, time)
+            controllerOutput.publish(motor_in, init_time)
             
         else:
             motor_in = 0
-            controllerOutput.publish(motor_in, time)
+            controllerOutput.publish(motor_in, init_time)
         
         rate.sleep()
-    #rospy.spin()
-
-
-
