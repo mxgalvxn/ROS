a79d2c3378b03b115dc75678c9c34d01ac2747f5
Manuel Ochoa <83720461+Manuelo247@users.noreply.github.com>
2023-02-27 22:20:46 -0600

    Add files via upload

--- control/scripts/controller_node.py
+++ control/scripts/controller_node.py
@@ -10,31 +10,40 @@ motor_init = False
 ref = 0
 
 def receive_feedback(output):
+    global motor_out 
+    global motor_init 
     motor_out = output
-    motor_init = True
+    motor_init= True
+    
 
-def receive_setpoint(data):
-    ref = data
+def receive_setpoint(refSetPoint):
+    ref = refSetPoint.data
+    
 
     
 if __name__ == '__main__':
     rospy.init_node("controller_node")
 
-    rate = rospy.get_param("/system_node_rate")
+    rateParam = rospy.get_param("/system_node_rate")
+    rate = rospy.Rate(rateParam)
     kp = rospy.get_param("/Kp")
     Ti = rospy.get_param("/Ti")
     Td = rospy.get_param("/Td")
     ref = rospy.get_param("/set_point")
 
-    systemfeedback = rospy.Subscriber("/motor_out",Float32, receive_feedback)
-    controllerOutput = rospy.Publisher("/motor_input",Float32, queue_size=10)
-    setPointSubscr = rospy.Subscriber("/motor_out",Float32,receive_setpoint)
-    handler=rospy.Subscriber("/setpoint",Float32, queue_size=10)
+    systemfeedback = rospy.Subscriber("/motor_output",motor_output, receive_feedback)
+    controllerOutput = rospy.Publisher("/motor_input",motor_input, queue_size=10)
+    setPointSubscr = rospy.Subscriber("/setpoint",Float32, receive_setpoint)
+
     init_time = rospy.get_time()
     lastError = 0.0
     cumError = 0.0
     motor_in = 0.0
+
     controllerOutput.publish(motor_in)
+    rospy.loginfo("The signal value is: %f", motor_in)
+    
+    
     while not rospy.is_shutdown():
         time = rospy.get_time()-init_time
         dTime = rospy.Time.now().to_sec() - time
@@ -49,8 +58,9 @@ if __name__ == '__main__':
         else:
             motor_in = 0
             controllerOutput.publish(motor_in)
-    rospy.spin()
-    rate.sleep()
+        
+        rate.sleep()
+    #rospy.spin()
 
 
 
--- control/scripts/set_point_generator.py
+++ control/scripts/set_point_generator.py
@@ -3,23 +3,24 @@ import rospy
 import numpy as np
 import math as m
 
-from std_msgs import Float32
+from std_msgs.msg import Float32
 
 
-if __name__ == '__main__':
-  rospy.init_node("setpoint_generator")
-    
-  setPointPublisher =rospy.Publisher("/setpoint",Float32, queue_size=10)    
-  init_time = rospy.get_time()
-
-  rate = rospy.get_param("/system_node_rate")
-
-  while not rospy.is_shutdown():
-    
-    time = rospy.get_time()-init_time
-    refVal = abs(3*m.tan(pow(m.log(1/m.exp(m.sin(time*0.3))),3))+m.cos(time*0.3))
-    setPointPublisher.publish(refVal);
-    rospy.loginfo("The signal value is: %f at a time %f", refVal, time)
-    rate.sleep()
+rospy.init_node("setpoint_generator")
   
+setPointPublisher =rospy.Publisher("/setpoint",Float32, queue_size=10)    
+init_time = rospy.get_time()
+
+  
+rate = rospy.Rate(10000)
+nodeRate = rospy.get_param("/system_node_rate")
+
+
+while not rospy.is_shutdown():
+  
+  time = rospy.get_time()-init_time
+  refVal = abs(3*m.tan(pow(m.log(1/m.exp(m.sin(time*0.3))),3))+m.cos(time*0.3))
+  setPointPublisher.publish(refVal);
+  rate.sleep()
+ 
 
