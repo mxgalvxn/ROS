e3565936155d203936dffff197efbc4e98b196a0
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-03-14 16:50:48 -0600

    odiomivida

--- challenge5/close_loop.py
+++ challenge5/close_loop.py
@@ -1,76 +1,89 @@
-#!/usr/bin/env python
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <std_msgs/Float32.h>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include "final/motor_input.h"
+#include "final/motor_output.h"
+#include "final/sender.h"
 
-import rospy
-from std_msgs.msg import Float32, Float64
 
-KP = 0.1
-KI = 0.01
-KD = 0.01
+const double KP = 2;
+const double KI = 0.28;
+const double KD = 0;
 
-MIN_PWM = 0.0
-MAX_PWM = 1.0
+const double MIN_PWM = 0.0;
+const double MAX_PWM = 1.0;
 
-MIN_SPEED = 0.0
-MAX_SPEED = 10.0
+const double MIN_SPEED = 0.0;
+const double MAX_SPEED = 10.0;
 
-target_speed = 0.0
-current_speed = 0.0
-error = 0.0
-last_error = 0.0
-integral = 0.0
-derivative = 0.0
-output = 0.0
+double target_speed = 0.0;
+double current_speed = 0.0;
+double error = 0.0;
+double last_error = 0.0;
+double integral = 0.0;
+double derivative = 0.0;
+double output = 0.0;
 
-node_rate = 100
+int nodeRate = 100;
 
-last_time = None
+ros::Time last_time;
 
-def speed_callback(msg):
-    global current_speed
-    current_speed = msg.data
+void speed_callback(const final::motor_output::ConstPtr &msg) {
+    current_speed = msg->output;
+}
 
-def control_loop(event):
-    global error, last_error, integral, derivative, output, last_time
+void set_point_callback(const final::sender::ConstPtr &setpoint) {
+    target_speed = setpoint->set_point_data;
+}
 
-    dt = (event.current_real - last_time).to_sec()
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "motor_controller");  
+    ros::NodeHandle handler;
 
-    # Calculate error
-    error = target_speed - current_speed
+    ros::Subscriber speedSub = handler.subscribe("/motor_output", 10, speed_callback);
+    ros::Subscriber setPointSub = handler.subscribe("/sender", 10, set_point_callback);
+    ros::Publisher signalPub = handler.advertise<final::motor_input>("/motor_input", 10);
 
-    # Calculate integral and derivative
-    integral += error * dt
-    derivative = (error - last_error) / dt
+    ros::Timer timer = handler.createTimer(ros::Duration(1.0 / nodeRate), [&](const ros::TimerEvent& event) {
+        double dt = (event.current_real - last_time).toSec();
 
-    # Calculate output
-    output = KP * error + KI * integral + KD * derivative
+        // Calculate error
+        error = target_speed - current_speed;
 
-    # Apply saturation limits
-    if output > MAX_PWM:
-        output = MAX_PWM
-    elif output < MIN_PWM:
-        output = MIN_PWM
+        // Calculate integral and derivative
+        integral += error * dt;
+        derivative = (error - last_error) / dt;
 
-    # Publish output
-    pwm_out = Float32()
-    pwm_out.data = output
-    signal_pub.publish(pwm_out)
+        // Calculate output
+        output = KP * error + KI * integral + KD * derivative;
 
-    # Store error for next iteration
-    last_error = error
+        // Apply saturation limits
+        if (output > MAX_PWM) {
+            output = MAX_PWM;
+        } else if (output < MIN_PWM) {
+            output = MIN_PWM;
+        }
 
-    # Store time for next iteration
-    last_time = event.current_real
+        // Publish output
+        std_msgs::Float32 pwmOut;
+        pwmOut.data = output;
+        signalPub.publish(pwmOut);
 
-if __name__ == '__main__':
-    rospy.init_node('sender')
+        // Store error for next iteration
+        last_error = error;
 
-    signal_pub = rospy.Publisher('/motor_input', Float32, queue_size=10)
-    speed_sub = rospy.Subscriber('/motor_speed', Float64, speed_callback)
+        // Store time for next iteration
+        last_time = event.current_real;
+    });
 
-    timer = rospy.Timer(rospy.Duration(1.0 / node_rate), control_loop)
+    last_time = ros::Time::now();
 
-    last_time = rospy.Time.now()
+    ros::param::get("/ref", target_speed);
 
-    target_speed = rospy.get_param('/ref')
+    ros::spin();
 
-    rospy.spin()
+    return 0;
+}
