1147855b86c256bef276612d7fb4856ae648ea76
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2023-03-15 20:53:28 -0600

    Update motor.ino

--- challenge5/motor.ino
+++ challenge5/motor.ino
@@ -1,48 +1,49 @@
-#include <ESP32Encoder.h>
 #include <ros.h>
 #include <final/motor_output.h>
 #include <final/motor_input.h>
 
-#define FWD_PIN_B 18
-#define BWD_PIN_B 15
-#define PWM_B 4
+#define MOTOR_ADE 18
+#define MOTOR_AT 15
+#define PWM 4
 
-#define ENCR_A 36
-#define ENCR_B 34
+#define ENC_A 34
+#define ENC_B 36
 #define PI 3.14159265358979
 
-const int    POLLING_TIME  = 100;
-const double TICKS_PER_REV = 737;
-const double RADS_PER_TICK = (2*PI)/TICKS_PER_REV;
+const int POLLING_TIME = 100;
+const double TICKS_PER_REV = 374;
+const double RADS_PER_TICK = (2 * PI) / TICKS_PER_REV;
 
 final::motor_output encB_vel;
 
-long currentPos = -999;
+long currentPos = 0;
 long oldPos = 0;
 long currTime = millis();
 
-ros::NodeHandle nh;
+ros::NodeHandle handler;
 
-ESP32Encoder encR;
+void enc() {
+  if (digitalRead(ENC_B))
+    currentPos++;
+  else
+    currentPos--;
+}
 
-void pwmCallback(const final::motor_input &pwmMsg){
-  ledcWrite(0, abs((int)(pwmMsg.input*255)));
-  if (pwmMsg.input > 0) {
-    digitalWrite(FWD_PIN_B, 1);
-    digitalWrite(BWD_PIN_B, 0);
+void pwmCallback(const final::motor_input &pwmIn) {
+  ledcWrite(0, abs((int)(pwmIn.input * 180)));
+  if (pwmIn.input > 0) {
+    digitalWrite(MOTOR_ADE, 180);
+    digitalWrite(MOTOR_AT, 0);
   } else {
-    digitalWrite(FWD_PIN_B, 0);
-    digitalWrite(BWD_PIN_B, 1);
+    digitalWrite(MOTOR_ADE, 0);
+    digitalWrite(MOTOR_AT, 180);
   }
 }
 
-void calculateSpeed(int newPos){
-  Serial.println(newPos);
-  if (newPos != currentPos){
-    currentPos = newPos;
-  }
-  encB_vel.output = float(((RADS_PER_TICK*(currentPos - oldPos))*1000)/POLLING_TIME); // Rads per second
-  oldPos = currentPos;
+void calculateSpeed(int Pos) {
+  float angular_speed = float(((RADS_PER_TICK * (Pos - oldPos)) * 1000) / POLLING_TIME);
+  encB_vel.output = float(((RADS_PER_TICK * (Pos - oldPos)) * 1000) / POLLING_TIME); // Rads per second
+  oldPos = Pos;
 }
 
 ros::Subscriber<final::motor_input> pwm_receiver("/motor_input", pwmCallback);
@@ -51,28 +52,26 @@ ros::Publisher motor_velocity("/motor_output", &encB_vel);
 void setup() {
   final::motor_output motor_out;
   // Encoder initial setup
-  ESP32Encoder::useInternalWeakPullResistors=UP;
-  encR.attachHalfQuad(ENCR_A, ENCR_B);
-  encR.setCount(0);
-  encR.clearCount();
+  attachInterrupt(digitalPinToInterrupt(ENC_A), enc, RISING);
   // MOTOR_RIGHT config
   ledcSetup(0, 980, 8);
-  pinMode(FWD_PIN_B, OUTPUT);
-  pinMode(BWD_PIN_B, OUTPUT);
-  pinMode(PWM_B, OUTPUT);
+  pinMode(MOTOR_ADE, OUTPUT);
+  pinMode(MOTOR_AT, OUTPUT);
+  pinMode(PWM, OUTPUT);
+  pinMode(ENC_A, INPUT);
+  pinMode(ENC_B, INPUT);
   // ROS Init
-  ledcAttachPin(PWM_B, 0);
-  nh.initNode();
-  nh.subscribe(pwm_receiver);
-  nh.advertise(motor_velocity);
-  
+  ledcAttachPin(PWM, 0);
+  handler.initNode();
+  handler.subscribe(pwm_receiver);
+  handler.advertise(motor_velocity);
 }
 
 void loop() {
-  if(millis()- currTime >= POLLING_TIME){
-    calculateSpeed(encR.getCount());
+  if (millis() - currTime >= POLLING_TIME) {
+    calculateSpeed(currentPos);
     motor_velocity.publish(&encB_vel);
     currTime = millis();
   }
-  nh.spinOnce();
+  handler.spinOnce();
 }
